0d7f92330ef6a9da34e3dfc9d106994fa069f5c8
Kirsten Smith <brownsmith.kirsten@gmail.com>
2022-09-21 12:13:26 -0400

    Create randomized variables

---
+++ regRace2.js
@@ -0,0 +1,10 @@
+//create randomized variables
+let raceNumber = Math.floor(Math.random() * 1000);
+let runnerAge;
+do {
+  runnerAge = Math.floor(Math.random() * 70);
+} while (runnerAge < 10);
+
+//create class
+
+//print class object instance
